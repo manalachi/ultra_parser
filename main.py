3bee73532e5acd4cf3c6f1ee4da6db5dc9e0b2ce
MSerghei <seryga.ms@gmail.com>
2023-03-18 12:42:54 +0200

    refactoring block product_price

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ start_time = time.time()
 
 
 def get_data():
-    cur_time = datetime.datetime.now().strftime("%d_%m_%Y %H_%M")
-
-    with open(f"Товары по Акции_{cur_time}.csv", "w", encoding="cp1251", newline="") as file:
-        writer = csv.writer(file, delimiter=";")
-
-        writer.writerow(
-            {
-                "Продукт",
-                "Скидка",
-                "Цена",
-                "Ссылка"
-            }
-        )
+    # cur_time = datetime.datetime.now().strftime("%d_%m_%Y %H_%M")
+    #
+    # with open(f"Товары по Акции_{cur_time}.csv", "w", encoding="cp1251", newline="") as file:
+    #     writer = csv.writer(file, delimiter=";")
+    #
+    #     writer.writerow(
+    #         {
+    #             "Продукт",
+    #             "Скидка",
+    #             "Цена",
+    #             "Ссылка"
+    #         }
+    #     )
 
     headers = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
@@ -40,17 +40,24 @@ def get_data():
 
     # with open("index.html", encoding="utf-8") as file:
     #     src = file.read()
-    # soup = BeautifulSoup(src,"lxml")     
+    # soup = BeautifulSoup(src, "lxml")
 
     pages_count = int(
         soup.find('nav', attrs={"aria-label": "Pagination Navigation"}).find_all('div')[-1].find_all('span')[-1].text)
 
     products_data = []
-    for page in range(1, pages_count + 1):
-        # for page in range(5, 6):
+    # for page in range(1, pages_count + 1):
+    for page in range(9, 10):
         url = f"https://ultra.md/ru/promo/products?page={page}"
 
         response = requests.get(url=url, headers=headers)
+
+        # with open(f"index{page}.html", "w", encoding="utf-8") as file:
+        #     file.write(response.text)
+        # with open(f"index{page}.html", encoding="utf-8") as file:
+        #     src = file.read()
+        # soup = BeautifulSoup(src, "lxml")
+
         soup = BeautifulSoup(response.text, "lxml")
 
         products_items = soup.find('div', class_="products-list").find_all('div', class_="product-block-card-container")
@@ -65,7 +72,8 @@ def get_data():
 
             if product_data_sale is None:
                 try:
-                    product_title = product_data[0].find_all('a')[-1].text.strip().replace("ö", "o").replace("Wi‑Fi", "WiFi")
+                    product_title = product_data[0].find_all('a')[-1].text.strip().replace("ö", "o").replace("Wi‑Fi",
+                                                                                                             "WiFi")
                 except:
                     product_title = "No data available!"
 
@@ -82,28 +90,25 @@ def get_data():
                 except:
                     product_discount = "No discount!"
 
-                try:
-                    if len(product_data) == 23:
-                        product_price = product_data[14].find('div', class_="relative mt-1 w-full").find('span',
-                                                                                                         class_="text-xl").text.strip().replace(
-                            " ", "").replace("\n", " ")
-                        # product_price_re = re.split(r'\s+', product_price)
-                        # product_price = " ".join(product_price_re)
-                    elif len(product_data) == 25:
-                        product_price = product_data[16].find('div', class_="relative mt-1 w-full").find('span',
-                                                                                                         class_="text-xl").text.strip().replace(
-                            " ", "").replace("\n", " ")
-                        # product_price_re = re.split(r'\s+', product_price)
-                        # product_price = " ".join(product_price_re)
-                    else:
-                        product_price = product_data[12].find('div', class_="relative mt-1 w-full").find('span',
-                                                                                                         class_="text-xl").text.strip().replace(
-                            " ", "").replace("\n", " ")
-                        # product_price_re = re.split(r'\s+', product_price)
-                        # product_price = "".join(product_price_re)
-                except:
-                    product_price = "No data available!"
+                # ---------------Product price
+                dict_param_div = {22: 16, 23: 14, 25: 16}
 
+                for k, v in dict_param_div.items():
+                    try:
+                        if len(product_data) == k:
+                            product_price = product_data[v].find('div', class_="relative mt-1 w-full").find('span',
+                                                                                                            class_="text-xl").text.strip().replace(
+                                " ", "").replace("\n", " ")
+                        else:
+                            product_price = product_data[12].find('div', class_="relative mt-1 w-full").find('span',
+                                                                                                             class_="text-xl").text.strip().replace(
+                                " ", "").replace("\n", " ")
+                            # product_price_re = re.split(r'\s+', product_price)
+                            # product_price = "".join(product_price_re)
+                    except:
+                        product_price = "No data available!"
+
+                # ---------------Product link
                 try:
                     product_link = product_data[0].find_all('a')[-1].get('href')
                 except:
@@ -117,49 +122,52 @@ def get_data():
                         "URL-LINK": product_link
                     }
                 )
-                try:
-                    with open(f"Товары по Акции_{cur_time}.csv", "a", encoding="cp1251", newline="") as file:
-                        writer = csv.writer(file, delimiter=";")
+                print(len(product_data))
+                print(product_price)
 
-                        writer.writerow(
-                            (
-                                product_title,
-                                product_discount,
-                                product_price,
-                                product_link
-                            )
-                        )
-                except Exception as err:
-                    print(err)
-            else:
-                products_data.append(
-                    {
-                        "Title": product_data_sale,
-                        "Discount": product_data_sale,
-                        "Price": product_price,
-                        "URL-LINK": product_link
-                    }
-                )
-                try:
-                    with open(f"Товары по Акции_{cur_time}.csv", "a", encoding="cp1251", newline="") as file:
-                        writer = csv.writer(file, delimiter=";")
-
-                        writer.writerow(
-                            (
-                                product_title,
-                                product_data_sale,
-                                product_price,
-                                product_link
-                            )
-                        )
-                except Exception as err:
-                    print(err)
+            #     try:
+            #         with open(f"Товары по Акции_{cur_time}.csv", "a", encoding="cp1251", newline="") as file:
+            #             writer = csv.writer(file, delimiter=";")
+            #
+            #             writer.writerow(
+            #                 (
+            #                     product_title,
+            #                     product_discount,
+            #                     product_price,
+            #                     product_link
+            #                 )
+            #             )
+            #     except Exception as err:
+            #         print(err)
+            # else:
+            #     products_data.append(
+            #         {
+            #             "Title": product_data_sale,
+            #             "Discount": product_data_sale,
+            #             "Price": product_price,
+            #             "URL-LINK": product_link
+            #         }
+            #     )
+            #     try:
+            #         with open(f"Товары по Акции_{cur_time}.csv", "a", encoding="cp1251", newline="") as file:
+            #             writer = csv.writer(file, delimiter=";")
+            #
+            #             writer.writerow(
+            #                 (
+            #                     product_title,
+            #                     product_data_sale,
+            #                     product_price,
+            #                     product_link
+            #                 )
+            #             )
+            #     except Exception as err:
+            #         print(err)
 
         print(f"Обработана {page} / {pages_count}")
         time.sleep(2)
 
-    with open(f"Товары по Акции_{cur_time}.json", "w", encoding="utf-8") as file:
-        json.dump(products_data, file, indent=4, ensure_ascii=False)
+    # with open(f"Товары по Акции_{cur_time}.json", "w", encoding="utf-8") as file:
+    #     json.dump(products_data, file, indent=4, ensure_ascii=False)
 
 
 def main():
